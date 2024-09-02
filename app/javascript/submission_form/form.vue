4e1266a772f150ea11b58f207e13b452b9343f17
Sigma9 <imdm@sigma9.pt>
2024-09-02 15:45:28 +0100

    i18n changes

--- app/javascript/submission_form/form.vue
+++ app/javascript/submission_form/form.vue
@@ -277,7 +277,6 @@
                     </span>
                   </label>
                 </div>
-                <button @click="uncheckAll(currentField)">Uncheck all</button>
               </div>
             </div>
           </div>
@@ -1199,9 +1198,6 @@ export default {
       } else {
         return `${this.t('option')} ${index + 1}`
       }
-    },
-    uncheckAll(currentField) {
-      return currentField.options.map( ({value}) => value = null)
     },
     maybeTrackEmailClick () {
       const { queryParams } = this
